57dbef834fc5cfe69513975a5e84f5fdc8d9e441
Cameron Gallarno <cgallarno@gmail.com>
2015-03-15 03:03:34 -0400

    new Site

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+    livereload = require('gulp-livereload');
+
+gulp.task('html', function() {
+    return gulp.src('./*.html')
+        .pipe(gulp.dest(''))
+        .pipe(livereload());
+});
+
+gulp.task('img', function() {
+	return gulp.src('./img/**')
+				.pipe(livereload());
+});
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('*.html', ['html']);
+  gulp.watch('img/**', ['img']);
+});
